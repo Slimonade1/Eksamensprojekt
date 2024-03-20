ddee1b754e896d18be5fb9e3517d92b60e3b2694
Marinus Christensen <marinus@thorbjorn.dk>
2024-03-20 12:22:22 +0100

    WHAM

--- Scripts/CharacterDescription.gd
+++ Scripts/CharacterDescription.gd
@@ -1,5 +1,12 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var is_paused = false setget set_is_paused
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.is_paused = !is_paused
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	visible = is_paused
