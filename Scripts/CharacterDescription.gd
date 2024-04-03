36d9cfcc6b2d793251eaebadb72bf970434f777e
Marinus Christensen <marinus@thorbjorn.dk>
2024-04-03 13:21:02 +0200

    infoskreen

--- Scripts/CharacterDescription.gd
+++ Scripts/CharacterDescription.gd
@@ -4,9 +4,11 @@ var is_paused = false setget set_is_paused
 onready var description = $CanvasLayer
 onready var showing = false
 
-func _unhandled_input(event):
+func _process(delta):
 	if showing:
-		self.is_paused = !is_paused
+		self.is_paused = true
+		set_is_paused(self.is_paused)
+		print("OMG CharacterDescription Showings", is_paused )
 
 func set_is_paused(value):
 	is_paused = value
